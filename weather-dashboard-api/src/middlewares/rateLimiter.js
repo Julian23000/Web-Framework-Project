3931f90ed5a48f8b5874474d825456f9b77c83a5
Nikhil - <127117742+nikv844@users.noreply.github.com>
2025-05-06 21:33:49 +0300

    Added main front-end of the API website

--- weather-dashboard-api/src/middlewares/rateLimiter.js
+++ weather-dashboard-api/src/middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 const weatherRateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 5, // Limits each IP to 5 requests per minute
+  max: 10, // Limits each IP to 5 requests per minute
   message: 'Too many requests, please try again later.',
 });
 module.exports = { weatherRateLimiter };
